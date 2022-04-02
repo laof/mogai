b7a64b509d98960e85aa8875aa35114036e172ed
laof <345603828@qq.com>
2022-04-02 13:31:01 +0800

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'no-routing'`, () => {
+  it(`should have as title 'mogai'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('no-routing');
+    expect(app.title).toEqual('mogai');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('no-routing app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mogai app is running!');
   });
 });
